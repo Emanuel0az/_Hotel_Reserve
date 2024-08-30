6a12169ee51d531c7ecf0f4660f5a40df0014f73
Emanuel0az <168204486+Emanuel0az@users.noreply.github.com>
2024-08-30 10:52:21 -0600

    se agrega un trigger que al actualizar una fecha de salida de una reserva cambia el estado de la habitacion

--- 5_triggers.sql
+++ 5_triggers.sql
@@ -35,3 +35,33 @@ BEGIN
         SET MESSAGE_TEXT = 'Esta habitación ya está ocupada';
     END IF;
 END;
+
+
+
+
+CREATE TRIGGER actualizar_reserva_disponible
+AFTER UPDATE ON Reservas
+FOR EACH ROW
+BEGIN
+    IF CURDATE() > NEW.fecha_salida THEN
+        UPDATE Habitaciones
+        SET Disponibilidad = 'Disponible'
+        WHERE Num_HabitacionA = NEW.id_habitacion;
+    ELSEIF CURDATE() < NEW.fecha_entrada THEN
+        UPDATE Habitaciones
+        SET Disponibilidad = 'Reservado'
+        WHERE Num_HabitacionA = NEW.id_habitacion;
+    ELSE
+        UPDATE Habitaciones
+        SET Disponibilidad = 'Ocupado'
+        WHERE Num_HabitacionA = NEW.id_habitacion;
+    END IF;
+END;
+
+UPDATE Reservas
+SET fecha_salida = '2023-03-10'
+WHERE id_habitacion = 10;
+
+SELECT * from reservas
+
+SELECT * FROM Habitaciones WHERE Num_HabitacionA = ?;
